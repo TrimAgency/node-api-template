6bdbba98eb00dc6fb1733ce614dee5fa7ee5f3b7
Stephanie Ricardo <stephanie.ricardo1@gmail.com>
2018-01-26 16:18:40 -0500

    Adds jasmine, db helper, auth helper, and login specs

---
+++ spec/helpers/auth_helpers.ts
@@ -0,0 +1,18 @@
+import { default as User, UserModel } from "../../src/models/user.model";
+import { createJWT, JWT } from "../../src/controllers/login.controller";
+
+export const getUser = () => {
+  return new Promise<UserModel>((res, rej) => {
+    User.findOne()
+        .sort({ field: "asc", _id: -1 })
+        .limit(1)
+        .exec((err, doc: UserModel) => { res(doc); });
+  });
+};
+
+export const userJwt = (user: UserModel) => {
+  return new Promise<JWT>( async(res, rej) => {
+    const jwt = await createJWT(user);
+    res(jwt);
+  });
+};
---
+++ spec/helpers/db.helper.ts
@@ -0,0 +1,38 @@
+import * as mongoose from "mongoose";
+// fixtures
+import { users } from "../fixtures/users.fixture";
+
+// models
+import { default as User, UserModel } from "../../src/models/user.model";
+
+export interface ModelSeed {
+  name: mongoose.Model<any>;
+  data: Array<any>;
+}
+
+export interface Models {
+  [key: string]: ModelSeed;
+}
+
+const models: Models = { "User": { name: User, data: users } };
+
+export const dropDB = (): void => {
+  for (const key in models) {
+    const model = models[key].name;
+    model.collection.drop().then( (data) => { console.log("dropDb", data); } );
+  }
+};
+
+export const seedModel = (name: string) => {
+  return new Promise<UserModel>( (res, rej) => {
+    models[name].data.forEach( (item, index) => {
+      const count = index + 1;
+      const length = models[name].data.length;
+      models[name].name.create(item, (err, doc: UserModel) => {
+        if (count === length) {
+          res(doc);
+        }
+      });
+    });
+  });
+};
