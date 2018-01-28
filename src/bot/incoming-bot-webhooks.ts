cd65c0102ce324cfaf74c222de5070e0631f0c52
Dominique Miller <dominique.p.miller@gmail.com>
2018-01-27 20:29:31 -0500

    Remove reference to original botkit.d.ts.  In place simply extend the Botkit.SlackController interface.  Update Incoming webhooks file to use new interface.

--- src/bot/incoming-bot-webhooks.ts
+++ src/bot/incoming-bot-webhooks.ts
@@ -1,5 +1,3 @@
-import * as Botkit from "botkit";
-import { botConfigController } from "../bot/bot";
 import { Response, Request, NextFunction } from "express";
 import { config } from "../config/config";
 import { SlackControllerExtended } from "../botkit-extend";
@@ -10,7 +8,7 @@ const env = process.env.NODE_ENV;
 export const incomingBotWebhooks = (req: Request,
                                     res: Response,
                                     next: NextFunction,
-                                    botConfigController: any) => {
+                                    botConfigController: SlackControllerExtended ) => {
 
   if (config[env].bot.SLACK_VERIFICATION_TOKEN === req.body.token || JSON.parse(req.body.payload).token) {
     // Response to slack that webhook was recevied
