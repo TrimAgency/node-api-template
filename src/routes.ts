2ad671edb27509056f921772bcbd943746e7e643
Stephanie Ricardo <stephanie.ricardo1@gmail.com>
2018-01-29 19:12:27 -0500

    Finishes sign up route

--- src/routes.ts
+++ src/routes.ts
@@ -1,15 +1,17 @@
 import { Express } from "express";
 import * as passport from "passport";
-// import { incomingBotWebhooks } from "./bot/incoming-bot-webhooks";
-import * as loginController from "./controllers/login.controller";
 import { errorHandler } from "./middleware/error.handler";
+// import { incomingBotWebhooks } from "./bot/incoming-bot-webhooks";
+
+import * as loginController from "./controllers/login.controller";
+import * as signupController from "./controllers/signup.controller";
 
 export function routes(app: Express) {
   // add routes here
 
   app.post("/api/login", loginController.login);
 
-  app.post("/api/user", loginController.create);
+  app.post("/api/users", signupController.create);
 
   // To use the bot
   // app.post("/slack/receive", incomingBotWebhooks);
