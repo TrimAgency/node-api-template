414d6ecb8158722303054e2905dd0acb5d0bafbc
Stephanie Ricardo <stephanie.ricardo1@gmail.com>
2018-02-16 16:05:17 -0500

    Finishes onboarding controller

--- src/bot-controllers/onboarding.bot.controller.ts
+++ src/bot-controllers/onboarding.bot.controller.ts
@@ -4,7 +4,7 @@ import * as Botkit from "botkit";
 export const onboarding = (botConfigController: Botkit.SlackController) => {
   botConfigController.on("onboard", (bot: any) => {
       console.log("Starting an onboarding experience!");
-      console.log("**Bot**", bot);
+
       // This message can be customized to share instructions for the team admin
       bot.startPrivateConversation({user: bot.config.createdBy}, (err: Error, convo: any) => {
         if (err) { console.log("Could not send onboarding message", err); }
